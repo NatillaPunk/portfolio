9d99c98336333a3c3493227af4ee0b9eec17aef2
NatillaPunk <damelendezq@gmail.com>
2025-06-01 21:49:08 -0600

    feat: implement responsive design for landing page sections; add mobile styles for profile summary and CV summary

--- js/js.js
+++ js/js.js
@@ -1,11 +1,28 @@
-    
+
+
 
 //Sticky Profile Summary
-const nav_size = document.querySelector('.layout-navbar').offsetHeight;
-const profile_summary = document.querySelector('.layout-profile-summary');
 
-profile_summary.style.position = 'sticky';
-profile_summary.style.top = `${nav_size}px`;
+
+
+
+
+function setSticky(){
+const window_width = window.innerWidth;
+if (window_width > 768) {
+    const nav_size = document.querySelector('.layout-navbar').offsetHeight;
+    const profile_summary = document.querySelector('.layout-profile-summary');
+
+    profile_summary.style.position = 'sticky';
+    profile_summary.style.top = `${nav_size}px`;
+}
+
+}
+
+
+document.addEventListener('DOMContentLoaded', setSticky())
+document.addEventListener('resize', setSticky())
+
 
 
 
